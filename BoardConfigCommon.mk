a91fbf85fa57d5e98b8dce0cd2fcc3f1caee7249
Dan Cashman <dcashman@google.com>
2017-06-01 15:25:22 -0700

    msm8953-common: Add BOARD_PLAT_[PUBLIC|PRIVATE]_SEPOLICY_DIR
    Move vendor policy to vendor and add a place for system extensions.
    
    Bug: 38151691
    Bug: 62041272
    Test: Policy binary identical before and after
    Change-Id: Ie4e8527649787dcf2391b326daa80cf1c9bd9d2f
    (cherry picked from commit 2efafdd1074e66e9d3ae0b4c4914782c7b70c67c)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,7 +179,9 @@ TARGET_USES_OLD_MNC_FORMAT := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
