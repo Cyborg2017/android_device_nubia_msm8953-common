8b02d7b980e41305c423d747293e803d0c0a4fd9
Artem Borisov <dedsa2002@gmail.com>
2018-11-30 18:35:16 +0400

    msm8953-common: Update to P blobs
    * From xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.3.0.PDHMIFK:user/release-keys
    * Pin HIDL ANT+ libraries, as they don't exist in tissot
    * While we're at it, also add power-off alarm blobs
    
    Change-Id: I1397d24cfb6b1e8731bdc68542a427aca6338f8d

--- msm8953.mk
+++ msm8953.mk
@@ -290,6 +290,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     libcnefeatureconfig \
     librmnetctl \
     libxml2
