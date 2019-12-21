e9bad3ac732d2537b5733ff506fa339e44666e3c
chandra <chandra.chaganti@hotmail.com>
2019-12-21 14:31:33 +0530

    msm8953-common: Update SurfaceFlinger props
     * AOSP surfaceflinger has more smoother ui than that of CAF

--- prop.mk
+++ prop.mk
@@ -54,6 +54,13 @@ debug.sf.enable_hwc_vds=1 \
 ro.opengles.version=196610 \
 ro.sf.lcd_density=480
 
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.set_display_power_timer_ms=10000 \
+ro.surface_flinger.set_touch_timer_ms=5000 \
+ro.surface_flinger.set_idle_timer_ms=9000
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=1
