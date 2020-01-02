a80ab2341957578f96b95c9bcff45163a016d30a
Luca Stefani <luca020400@lineageos.org>
2020-01-02 07:53:14 +0000

    msm8953-common: Set persist.vendor.data.iwlan.enable=true

--- prop.mk
+++ prop.mk
@@ -111,6 +111,7 @@ ro.telephony.call_ring.multiple=false \
 persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
+persist.vendor.data.iwlan.enable=true \
 persist.vendor.data.mode=concurrent \
 persist.radio.apm_sim_not_pwdn=1
 
